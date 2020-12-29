9c2eef3215f203e138fa60318d22c7979f6bbb42
Niklas Fahl <nfahl2@ua.edu>
2020-12-28 18:56:08 -0600

    Added preliminary favicon files. Added more content to the hero section.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import Hero from './Hero';
-import ProfileImageVector from './resources/nik-fahl-vector.svg';
+// import ProfileImageVector from './resources/nik-fahl-vector.svg';
 
 function App() {
   return (
-    <div class="px-4 sm:px-6 md:px-8">
+    <div className="px-4 sm:px-6 md:px-8 pt-10">
       <Hero />
-      <div class="border-t border-gray-200 py-6 flex space-x-2 items-center justify-center -mx-4 px-4 sm:mx-0 sm:px-0">
-        <p class="text-gray-500 mr-2">© 2020 Niklas Fahl</p>
-        <img class="w-8 h-8 rounded-full" src={ProfileImageVector} alt="Logo"></img>
-      </div>
     </div>
+    // <div class="px-4 sm:px-6 md:px-8">
+    //   <Hero />
+    //   <div class="border-t border-gray-200 py-6 flex space-x-2 items-center justify-center -mx-4 px-4 sm:mx-0 sm:px-0">
+    //     <p class="text-gray-500 mr-2">© 2020 Niklas Fahl</p>
+    //     <img class="w-8 h-8 rounded-full" src={ProfileImageVector} alt="Logo"></img>
+    //   </div>
+    // </div>
   );
 }
 
