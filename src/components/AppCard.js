7d7909093e67771b83f705cf0f86525575a57167
Niklas Fahl <nfahl2@ua.edu>
2020-12-29 17:57:20 -0600

    Tweaked app icon size

--- src/components/AppCard.js
+++ src/components/AppCard.js
@@ -2,7 +2,7 @@ function AppCard({ name, clientName, appIcon, description, color, appStoreLink }
     return (
         <div className="flex flex-col max-w-screen-lg mx-auto rounded-3xl bg-gray-100 dark:bg-gray-1100 px-5 md:px-8 py-8 sm:py-14 md:py-16">
             <div className="mb-8 sm:mb-14 md:mb-16 mr-auto">
-                <img className="w-16 md:w-32 lg:w-36 h-16 md:h-32 lg:h-36" src={appIcon} alt="App Icon"></img>
+                <img className="w-16 sm:w-20 md:w-32 lg:w-36 h-16 sm:h-20 md:h-32 lg:h-36" src={appIcon} alt="App Icon"></img>
             </div>
 
             <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight mb-3 leading-none text-black dark:text-gray-100">{name}</h1>
